a6118c490a0240f223bbd506d61c5288d0d3e055
Quoc Khuong <khuonggm4869@gmail.com>
2025-04-20 07:16:55 +0700

    update speed for Devices

--- backend/src/models/Device.js
+++ backend/src/models/Device.js
@@ -11,6 +11,22 @@ const deviceSchema = new mongoose.Schema(
     status: { type: Boolean, default: false },
     isOnline: { type: Boolean, default: true },
     power: { type: Number, default: 0 }, // công suất (W)
+    speed: {
+      type: Number,
+      min: 0,
+      max: 100,
+      validate: {
+        validator: function (value) {
+          // Nếu type là 'fan' thì speed là bắt buộc
+          if (this.type === "fan") return value != null;
+          // Nếu không phải fan thì speed nên undefined hoặc null
+          return value == null;
+        },
+        message: function (props) {
+          return `Thuộc tính 'speed' chỉ áp dụng với thiết bị quạt (fan).`;
+        },
+      },
+    },
   },
   {
     timestamps: true,
